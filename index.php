802082f4b8df7d749d1006ef96afe55971db5a0b
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:36:35 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <?php
+    // Exercice 1
+    $object = 'Hello World';
+    $message = 'N\'importe quoi comme message lààààà';
+    ?>
+    <p>
+    <?php echo $object; ?>
+    </p>
+    <p>
+    <?php echo $message; ?>
+    </p>
+  </body>
+</html>
